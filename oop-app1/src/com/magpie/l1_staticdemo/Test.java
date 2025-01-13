ce16a7469c7aa3f512b18347690b25786226e268
yihuiba <yaohb@mail.ustc.edu.cn>
2025-01-13 14:55:44 +0800

    static 1.

---
+++ oop-app1/src/com/magpie/l1_staticdemo/Test.java
@@ -0,0 +1,11 @@
+package com.magpie.l1_staticdemo;
+
+public class Test {
+    public static void main(String[] args) {
+        new User();
+        new User();
+        new User();
+        new User();
+        System.out.println(User.number);
+    }
+}
---
+++ oop-app1/src/com/magpie/l1_staticdemo/User.java
@@ -0,0 +1,10 @@
+package com.magpie.l1_staticdemo;
+
+public class User {
+    // 类变量对外公开
+    public static int number;
+    public User(){
+        // User.number++;
+        number++;
+    }
+}
